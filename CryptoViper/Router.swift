f395b3d74ae85834b80b1161fb018e3f6c25a53e
iremkrkaplan <iremkrkaplan@gmail.com>
2025-07-27 13:01:21 +0300

    Router files code

--- CryptoViper/Router.swift
+++ CryptoViper/Router.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+
+//class, protocol
+//entryPoint
+//uygulama ilk açıldığında nereye gidecek gibi soruları cevaplar
+//VIP ı kontrol etmek için var
+
+protocol AnyRouter{
+    static func startExecution() -> AnyRouter
+}
+
+class CryptoRouter : AnyRouter {
+    
+    static func startExecution() -> any AnyRouter {
+        let router = CryptoRouter()
+        return router
+    }
+    
+    
+}
